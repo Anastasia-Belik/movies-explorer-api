767bc2f1e74d0f329a8ade612f0c9755ea08c25e
anastasia <nastja1994@yandex.ru>
2021-10-19 08:33:32 +0300

    feat: реализовать авторизацию

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { login, createUser } = require('./controllers/users');
 
 const { PORT = 3000 } = process.env;
 
@@ -8,6 +10,12 @@ const app = express();
 mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
   useNewUrlParser: true,
 });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/signin', login);
+app.post('/signup', createUser);
 
 app.use('/users', require('./routes/users'));
 app.use('/movies', require('./routes/movies'));
--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,8 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+
+const { NODE_ENV, JWT_SECRET = 'qwerty' } = process.env;
 
 function sendResponse(res, user) {
   if (user) {
@@ -18,6 +22,46 @@ function findUserByIdInDb(userId, res, next) {
       next(err);
     });
 }
+
+module.exports.createUser = (req, res, next) => {
+  const { email, password, name } = req.body;
+
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      name, email, password: hash,
+    }))
+    .then((user) => {
+      const userWithoutPassword = user.toObject();
+      delete userWithoutPassword.password;
+      res.send({ data: userWithoutPassword });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new Error('Переданы некорректные данные при создании пользователя'));
+      }
+      if (err.name === 'MongoServerError' && err.code === 11000) {
+        next(new Error('пользователь с таким email уже существует'));
+      }
+      next(err);
+    });
+};
+
+module.exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === 'production' ? JWT_SECRET : 'qwerty',
+        { expiresIn: '7d' },
+      );
+      res.send({ token });
+    })
+    .catch(() => {
+      next(new Error('передан неверный логин или пароль'));
+    });
+};
 
 module.exports.getMyInfo = (req, res, next) => {
   const userId = req.user._id;
